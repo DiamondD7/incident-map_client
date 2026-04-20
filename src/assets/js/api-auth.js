d53268fa53355581497e38f510827b4ec9137fd6
diamond <aaron.diamond2@yahoo.co.nz>
2026-04-21 01:07:51 +1200

    update name and link for backend

--- src/assets/js/api-auth.js
+++ src/assets/js/api-auth.js
@@ -1,5 +1,5 @@
 //const API_BASE_URL = 'https://localhost:7207/api/promotion';
-const API_BASE_URL = 'https://dealmap-d0awh8fve5geggb4.australiaeast-01.azurewebsites.net/api/promotion';
+const API_BASE_URL = 'https://hotspots-bzdmetbeg3aee6hk.australiasoutheast-01.azurewebsites.net/api/promotion';
 
 export const GetPromotions = `${API_BASE_URL}/all-promotions`;
 export const GetPromotionsByLocation = `${API_BASE_URL}/promotion-by-location`;
