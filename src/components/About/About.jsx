d7b2ff066c92e66efda9e3ff6901887719afd4fc
diamond <aaron.diamond2@yahoo.co.nz>
2026-07-18 23:19:02 +1200

    added settings container and contents

---
+++ src/components/About/About.jsx
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <div style={{ marginTop: "20px" }}>
+      <h1>About us</h1>
+      <p style={{ marginTop: "10px" }}>
+        <strong style={{ color: "#FA6737" }}>Hotspots</strong> is a discovery
+        platform built to connect people with the best food spots around them.{" "}
+        <br />
+        <br /> We combine curated locations, aesthetic experiences, and time
+        based deals into a single app,this makes it easier to decide where to go
+        faster. <br /> <br /> Beyond discovery, Hotspots supports local
+        businesses by helping them attract customers during quiet hours.
+        <br />
+        <br /> Our goal is simple:
+        <br /> make everyday decisions better for people, and more impactful for
+        businesses.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/About/AboutThisVersion.jsx
@@ -0,0 +1,46 @@
+const AboutThisVersion = () => {
+  return (
+    <div style={{ marginTop: "20px", height: "90dvh", overflow: "auto" }}>
+      <h1>About This Version</h1>
+      <div>
+        <p>
+          Welcome to Hotspots 👋 <br />
+          <br /> Hotspots is currently in its early stage, and we are working
+          hard to build a better way for you to discover cafes, restaurants, and
+          local spots around you. <br />
+          <br /> This version is our first step towards making it easier to find
+          places worth visiting. From hidden gems to spots offering special
+          deals and promotions.
+        </p>
+        <br />
+        <h3>Whats coming next</h3>
+        <p>
+          Hotspots is always improving. We are working on bringing more
+          features, such as: <br />
+          <br /> - A dedicated mobile app
+          <br /> - More ways to discover food spots
+          <br /> - Personalised recommendations and updates
+          <br /> - More local businesses and exclusive offers
+        </p>
+
+        <br />
+        <h3>A note from the founder</h3>
+        <p>
+          Hi there, you are using Hotspots during its early phases. Some
+          features may still be limited, and you may notice a lot of changes as
+          I continue improving your experience.
+          <br />
+          <br /> Your feedback really help shape what Hotspots becomes. Every
+          suggestion and visit helps us build a better platform for discovering
+          local hotspots. <br />
+          <br /> Thank you so much, appreciate it <br /> -💎
+        </p>
+        <br />
+        <br />
+        <br />
+      </div>
+    </div>
+  );
+};
+
+export default AboutThisVersion;
